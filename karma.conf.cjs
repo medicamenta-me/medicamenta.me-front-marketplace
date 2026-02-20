fd4567da0b199c8c7c82e91d6085c4fff81ca773
Gabriel SDG <gabrielsdg@users.noreply.github.com>
2026-02-19 22:42:20 -0300

    chore(ci): add karma config and update angular.json for CI
    - Create karma.conf.cjs with coverage reporting (JSON summary + LCOV)
    - Update angular.json to reference karma config for test runner

---
+++ karma.conf.cjs
@@ -0,0 +1,35 @@
+// Karma configuration file
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'json-summary', file: 'coverage-summary.json' },
+        { type: 'lcov' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
+  });
+};
